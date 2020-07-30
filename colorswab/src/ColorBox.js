9bf37bfd014894bc5e27676e5e32896a2b9f45eb
kylegreenf <kylegreenf@gmail.com>
2020-07-30 15:53:55 -0700

    Page number color dependant

--- colorswab/src/ColorBox.js
+++ colorswab/src/ColorBox.js
@@ -7,7 +7,7 @@ class ColorBox extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-        pageNumber : 0
+        pageNumber : 1
     };
 
   }
@@ -22,7 +22,8 @@ class ColorBox extends React.Component{
 
 
 
-  generateColorItem(color) {
+  generateColorItem(number) {
+      var color = colorJson[number*this.state.pageNumber].hexString;
       return (
         <div onClick={this.changeColor} className="Color-grid-item">
             <div className="Color-grid-item-color" style={{background: color}} />
@@ -36,18 +37,18 @@ class ColorBox extends React.Component{
     var color = data[3].hexString;
     return(
         <div className="Color-grid-container">
-            {this.generateColorItem("#cffff1")}
-            {this.generateColorItem(color)}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
-            {this.generateColorItem("blue")}
+            {this.generateColorItem(1)}
+            {this.generateColorItem(2)}
+            {this.generateColorItem(3)}
+            {this.generateColorItem(4)}
+            {this.generateColorItem(5)}
+            {this.generateColorItem(6)}
+            {this.generateColorItem(7)}
+            {this.generateColorItem(8)}
+            {this.generateColorItem(9)}
+            {this.generateColorItem(10)}
+            {this.generateColorItem(11)}
+            {this.generateColorItem(12)}
         </div>
     )
   }
